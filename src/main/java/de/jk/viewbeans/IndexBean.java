7299b95a3e46087f2b54477e8cc5b833b961a62f
ich <ich@ich-PC>
2014-01-01 20:56:34 +0100

    add pages

---
+++ src/main/java/de/jk/viewbeans/IndexBean.java
@@ -0,0 +1,25 @@
+package de.jk.viewbeans;
+
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class IndexBean implements Serializable {
+ 
+        private static final long serialVersionUID = 1L;
+ 
+        private String name;
+ 
+        public String getName() {
+                return name+"hdfdf";
+        }
+        public void setName(String name) {
+                this.name = name;
+        }
+
+}
